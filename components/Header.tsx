482f2aea5c94dd499f91d4dcdf6d534c7cd7308b
bspence205 <brianspencer@spencerbrian.com>
2022-09-21 00:51:54 -0700

    implement framer motion dependency, created hero component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from "framer-motion"
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20
-    xl:items-center'>
-        <div className='flex flex-row justify-between'>
+    <header className='sticky top-0 p-5 flex items-start justify-between 
+    max-w-7xl mx-auto z-20 xl:items-center'>
+      {/* framer motion */}
+        <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className='flex flex-row justify-between'>
         {/* Social Icon */}
 
         <SocialIcon url='https://github.com/bspence205'
@@ -24,10 +40,24 @@ export default function Header({}: Props) {
         fgColor='gray'
         bgColor='transparent'
         />
-        </div>
+        </motion.div>
 
 
-        <div className='flex flex-row items-center text-gray-300 cursor'>
+        <motion.div 
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className='flex flex-row items-center text-gray-300 cursor'>
         <SocialIcon 
         className='cursor-pointer'
         network="email"
@@ -38,7 +68,7 @@ export default function Header({}: Props) {
         Lets connect!
         </p>
 
-        </div>
+        </motion.div>
     </header>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
@@ -9,12 +10,13 @@ const Home: NextPage = () => {
         <title>Brian&apos;s Portfolio</title>
       </Head>
 
-
-      {/* Header */}
-
       <Header />
 
-      {/* Hero Section */}
+      {/* Hero */}
+
+      <section id="hero">
+        <Hero />
+      </section>
 
       {/* About */}
 
