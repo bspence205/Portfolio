b0c4804a064261b823b9a4c80d973cca8efc1ebf
bspence205 <brianspencer@spencerbrian.com>
2022-09-24 00:15:59 -0700

    set up hook to be used with type-writer to loop through

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import { Cursor, useTypewriter } from "react-simple-typewriter"
 
 type Props = {}
 
 function Hero({}: Props) {
+
+  const {text, count} = useTypewriter({
+    words: [
+      "Hi, My Name is Brian Spencer",
+      "Guy-Who-Loves-To-Code.tsx",
+      "<ButLovesHisCorgiMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  })
+
+
   return (
     <div>
-        
+      <p>{text}</p>
+      <Cursor />
     </div>
   )
 }
 
-export default Hero
+
+export default Hero;
